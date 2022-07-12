2c25e8e585438667406cd32e0156307630d29a38
Igor Kosulin <igor@kosulin.com>
2022-07-12 00:47:37 -0700

    Change package name

--- android/app/src/main/java/com/akellolcc/saufoto/newarchitecture/components/MainComponentsRegistry.java
+++ android/app/src/main/java/com/akellolcc/saufoto/newarchitecture/components/MainComponentsRegistry.java
@@ -1,4 +1,4 @@
-package com.saufoto.newarchitecture.components;
+package com.akellolcc.saufoto.newarchitecture.components;
 
 import com.facebook.jni.HybridData;
 import com.facebook.proguard.annotations.DoNotStrip;
